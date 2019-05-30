78ab06c1e188d7dd5fd8463694fae9cd933f18d9
Esther Li <li.esther2000@gmail.com>
2019-05-30 00:20:27 -0700

    Update parallel.c

--- Lab4/parallel.c
+++ Lab4/parallel.c
@@ -13,95 +13,70 @@
 
 void work_it_par(long *old, long *new) {
     int i, j, k;
-    int u, v, w;
+    int u;
     long compute_it, sum;
-    long aggregate=1.0;
+    long aggregate = 1.0;
+    long sum0 = 0.0;
+    long sum1 = 0.0;
+    long sum2 = 0.0;
+    long sum3 = 0.0;
+    long sum4 = 0.0;
+    long sum5 = 0.0;
+    long sum6 = 0.0;
+    long sum7 = 0.0;
+    long sum8 = 0.0;
+    long sum9 = 0.0;
+    const long a = we_need_the_func();
+    const long b = gimmie_the_func();
     
-    
-    
-#pragma omp parallel for firstprivate(old, new) private(i, j, k, compute_it, u, v, w) reduction (+:aggregate, sum)
-    for (i=1; i<DIM-1; i++) {
-        for (j=1; j<DIM-1; j++) {
-            for (k=1; k<DIM-1; k++) {
-                compute_it = old[i*DIM*DIM+j*DIM+k] * we_need_the_func() / gimmie_the_func();
+#pragma omp parallel for firstprivate(old, new) private(i, j, k, compute_it, u) reduction\
+(+:aggregate, sum, sum0, sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8, sum9)
+    for (i = 1; i < DIM - 1; i++) {
+        for (j = 1; j<DIM - 1; j++) {
+            for (k = 1; k < DIM - 1; k++) {
+                compute_it = old[i*DIM*DIM+j*DIM+k] * a / b;
                 aggregate += compute_it;
                 sum = 0.0;
                 for (u=-1; u<=1; u++) {
-                    for (v=-1; v<=1; v++) {
-                        for (w=-1; w<=1; w++) {
-                            sum+=old[(i+u)*DIM*DIM+(j+v)*DIM+(k+w)];
-                        }
-                    }
+                    sum+=old[(i+u)*DIM*DIM+(j-1)*DIM+(k-1)];
+                    sum+=old[(i+u)*DIM*DIM+(j-1)*DIM+(k)];
+                    sum+=old[(i+u)*DIM*DIM+(j-1)*DIM+(k+1)];
+                    sum+=old[(i+u)*DIM*DIM+(j)*DIM+(k-1)];
+                    sum+=old[(i+u)*DIM*DIM+(j)*DIM+(k)];
+                    sum+=old[(i+u)*DIM*DIM+(j)*DIM+(k+1)];
+                    sum+=old[(i+u)*DIM*DIM+(j+1)*DIM+(k-1)];
+                    sum+=old[(i+u)*DIM*DIM+(j+1)*DIM+(k)];
+                    sum+=old[(i+u)*DIM*DIM+(j+1)*DIM+(k+1)];
                 }
-                new[i*DIM*DIM+j*DIM+k]=sum/27;
+                sum /= 27;
+                new[i*DIM*DIM+j*DIM+k] = sum;
+                u = sum/100;
+                if (u<=0) sum0++;
+                else if (u == 1) sum1++;
+                else if (u == 2) sum2++;
+                else if (u == 3) sum3++;
+                else if (u == 4) sum4++;
+                else if (u == 5) sum5++;
+                else if (u == 6) sum6++;
+                else if (u == 7) sum7++;
+                else if (u == 8) sum8++;
+                else if (u>=9) sum9++;
             }
         }
     }
-    
     printf("AGGR:%ld\n",aggregate);
-    
-    /*
-     #pragma omp parallel for firstprivate(new, old) private(i, j, k) reduction (+:sum)
-     for (i=1; i<DIM-1; i++) {
-     for (j=1; j<DIM-1; j++) {
-     for (k=1; k<DIM-1; k++) {
-     sum = 0.0;
-     sum+=old[(i-1)*DIM*DIM+(j-1)*DIM+(k-1)];
-     sum+=old[(i-1)*DIM*DIM+(j-1)*DIM+(k)];
-     sum+=old[(i-1)*DIM*DIM+(j-1)*DIM+(k+1)];
-     sum+=old[(i-1)*DIM*DIM+(j)*DIM+(k-1)];
-     sum+=old[(i-1)*DIM*DIM+(j)*DIM+(k)];
-     sum+=old[(i-1)*DIM*DIM+(j)*DIM+(k+1)];
-     sum+=old[(i-1)*DIM*DIM+(j+1)*DIM+(k-1)];
-     sum+=old[(i-1)*DIM*DIM+(j+1)*DIM+(k)];
-     sum+=old[(i-1)*DIM*DIM+(j+1)*DIM+(k+1)];
-     sum+=old[(i)*DIM*DIM+(j-1)*DIM+(k-1)];
-     sum+=old[(i)*DIM*DIM+(j-1)*DIM+(k)];
-     sum+=old[(i)*DIM*DIM+(j-1)*DIM+(k+1)];
-     sum+=old[(i)*DIM*DIM+(j)*DIM+(k-1)];
-     sum+=old[(i)*DIM*DIM+(j)*DIM+(k)];
-     sum+=old[(i)*DIM*DIM+(j)*DIM+(k+1)];
-     sum+=old[(i)*DIM*DIM+(j+1)*DIM+(k-1)];
-     sum+=old[(i)*DIM*DIM+(j+1)*DIM+(k)];
-     sum+=old[(i)*DIM*DIM+(j+1)*DIM+(k+1)];
-     sum+=old[(i+1)*DIM*DIM+(j-1)*DIM+(k-1)];
-     sum+=old[(i+1)*DIM*DIM+(j-1)*DIM+(k)];
-     sum+=old[(i+1)*DIM*DIM+(j-1)*DIM+(k+1)];
-     sum+=old[(i+1)*DIM*DIM+(j)*DIM+(k-1)];
-     sum+=old[(i+1)*DIM*DIM+(j)*DIM+(k)];
-     sum+=old[(i+1)*DIM*DIM+(j)*DIM+(k+1)];
-     sum+=old[(i+1)*DIM*DIM+(j+1)*DIM+(k-1)];
-     sum+=old[(i+1)*DIM*DIM+(j+1)*DIM+(k)];
-     sum+=old[(i+1)*DIM*DIM+(j+1)*DIM+(k+1)];
-     sum/=27;
-     new[i*DIM*DIM+j*DIM+k] = sum;
-     }
-     }
-     }
-     */
-    
-    /*
-     #pragma omp parallel for firstprivate(new, old) private(i, j, k, u, v, w) reduction(+:sum)
-     for (i=1; i<DIM-1; i++) {
-     for (j=1; j<DIM-1; j++) {
-     for (k=1; k<DIM-1; k++) {
-     
-     }
-     }
-     }
-     
-     */
-    
-#pragma omp parallel for firstprivate(new, histogrammy) private(u, i, j, k)
-    for (i=1; i<DIM-1; i++) {
-        for (j=1; j<DIM-1; j++) {
-            for (k=1; k<DIM-1; k++) {
-                u=(new[i*DIM*DIM+j*DIM+k]/100);
-                if (u<=0) u=0;
-                if (u>=9) u=9;
-                histogrammy[u]++;
-            }
-        }
-    }
+#pragma omp parallel num_threads(10)
+    histogrammy[0] += sum0;
+    histogrammy[1] += sum1;
+    histogrammy[2] += sum2;
+    histogrammy[3] += sum3;
+    histogrammy[4] += sum4;
+    histogrammy[5] += sum5;
+    histogrammy[6] += sum6;
+    histogrammy[7] += sum7;
+    histogrammy[8] += sum8;
+    histogrammy[9] += sum9;
 }
+
+
 
